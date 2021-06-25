c28304eb982b90ed2b8f2f40ada07ccaf1450fc3
marzieh mousavi <smsmoosavi1999@gmail.com>
2021-06-25 13:30:30 +0430

    UI finished

---
+++ chatroom/index.php
@@ -0,0 +1,156 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" type="text/css">
+  </head>
+  <script src="chat.js"></script>
+  <body>
+
+<?php
+ $con = mysqli_connect("localhost", "root", "", "chat");
+
+	$query = "SELECT * FROM users  ";
+	$result = mysqli_query($con, $query);
+	$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// echo "<div class='main content'>";
+		
+	// foreach ($data as $key => $row) {
+	// 	echo "<ul>";
+	// 	foreach ($row as $key2 => $row2) {
+	// 		echo "<li><img src='" . $row2 . "' alt='avatar' /></li>";
+	// 	}
+	// 	echo "</ul>";
+	// }
+	// echo "</div>";
+
+
+	$con->close();
+
+?>
+
+
+    <div class="list">
+      <div class="people-list" id="people-list">
+        <div class="profile">
+          
+            <img src="./resourses/1.jpg" alt="avatar" />
+            <div class="about">
+              <div class="myname" >Vincent Porter</div>
+              <div class="logout"><div class="mystatus">online</div>
+              <button class="logout-btn">logout</button>
+            </div></div>
+          
+        </div>
+        <ul class="people">
+
+          <li class="">
+            <img src="./resourses/2.jpg" alt="avatar" />
+            <div class="about">
+              <div class="name">Aiden Chavez</div>
+              <div class="status">left 7 mins ago</div>
+            </div>
+          </li>
+
+          <li class="">
+            <img src="./resourses/3.jpg" alt="avatar" />
+            <div class="about">
+              <div class="name">Mike Thomas</div>
+              <div class="status">online</div>
+            </div>
+          </li>
+
+          <li class="">
+            <img src="./resourses/4.jpg" alt="avatar" />
+            <div class="about">
+              <div class="name">Erica Hughes</div>
+              <div class="status">online</div>
+            </div>
+          </li>
+
+          <li class="">
+            <img src="./resourses/5.jpg" alt="avatar" />
+            <div class="about">
+              <div class="name">Ginger Johnston</div>
+              <div class="status">online</div>
+            </div>
+          </li>
+
+          <li class="">
+            <img src="./resourses/6.jpg" alt="avatar" />
+            <div class="about">
+              <div class="name">Ginger Johnston</div>
+              <div class="status">online</div>
+            </div>
+          </li>
+          <li class="">
+            <img src="./resourses/7.jpg" alt="avatar" />
+            <div class="about">
+              <div class="name">Ginger Johnston</div>
+              <div class="status">online</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="msger">
+      <header class="msger-header">
+        <div class="msger-header-title">
+          <i class="fas fa-comment-alt"></i> SMSM messenger
+        </div>
+        <div class="msger-header-options">
+          <span><i class="fa fa-cog"></i></span>
+        </div>
+      </header>
+
+      <main class="msger-chat">
+        <div class="msg left-msg">
+          <div
+            class="msg-img"
+            style="background-image: url(./resourses/1.jpg)"
+          ></div>
+
+          <div class="msg-bubble">
+            <div class="msg-info">
+              <div class="msg-info-name">Mahsa</div>
+              <div class="msg-info-time">12:45</div>
+            </div>
+
+            <div class="msg-text">
+              Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
+            </div>
+          </div>
+        </div>
+
+        <div class="msg right-msg">
+          <div
+            class="msg-img"
+            style="background-image: url(./resourses/2.jpg)"
+          ></div>
+
+          <div class="msg-bubble">
+            <div class="msg-info">
+              <div class="msg-info-name">Marzieh</div>
+              <div class="msg-info-time">12:46</div>
+            </div>
+
+            <div class="msg-text">You can change your name in JS div!</div>
+          </div>
+        </div>
+      </main>
+
+      <form class="msger-inputarea">
+        <input
+          type="text"
+          class="msger-input"
+          placeholder="Enter your message..."
+        />
+        
+        
+        <button type="submit" class="msger-record-btn"><i class="fa fa-microphone"></i></button>
+        <button type="submit" class="msger-send-btn"><i class="fa fa-send"></i></button>
+      </form>
+    </div>
+  </body>
+</html>
---
+++ chatroom/initialization/tableCreate.php
@@ -0,0 +1,81 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "chat");
+if (!$con) {
+    die('Could not connect: ' . mysqli_connect_error());
+}
+
+//---------------------------------------------
+
+$sql = "CREATE TABLE users
+(
+user varchar(15) NOT NULL , 
+PRIMARY KEY(user),
+pass varchar(15) NOT NULL ,
+picture BLOB 
+)";
+
+//---------------------------------------------
+
+// $sql = "CREATE TABLE Students
+// (
+// user varchar(15) NOT NULL , 
+// PRIMARY KEY(user),
+// pass varchar(15) NOT NULL ,
+// lvl int NOT NULL ,
+// dailyCorrects int ,
+// lastAccess DATETIME,
+// corrects varchar(15),
+// wrongs varchar(15)
+// )";
+
+//---------------------------------------------
+
+// $sql = "CREATE TABLE Questions
+// (
+// Qid int NOT NULL AUTO_INCREMENT , 
+// PRIMARY KEY(Qid),
+// qustion varchar(15) ,
+// objPic BLOB ,
+// objsPic BLOB ,
+// oprand varchar(5),
+// anwser int,
+// lvl int 
+// )";
+
+//---------------------------------------------
+
+// $sql = "CREATE TABLE objPictures
+// (
+// OPid int NOT NULL AUTO_INCREMENT , 
+// PRIMARY KEY(OPid),
+// objPic BLOB 
+// )";
+
+////bayad dar phpmyadmin be soorat dasti 8 ax ro insert kard
+////ax ha dar folder "picture for insert in db"
+
+//---------------------------------------------
+
+
+// $sql = "CREATE TABLE objsPictures
+// (
+// OsPid int NOT NULL AUTO_INCREMENT , 
+// PRIMARY KEY(OsPid),
+// objsPic BLOB 
+// )";
+
+////bayad dar phpmyadmin be soorat dasti 3 ax ro insert kard
+////ax ha dar folder "picture for insert in db"
+
+//---------------------------------------------
+
+if (mysqli_query($con, $sql)) {
+    echo "Table created";
+} else {
+    echo "Error creating table: " . mysqli_error($con);
+}
+mysqli_close($con);
+
+
+
+// http://localhost/PHP/project/v3/initialization/tableCreate.php
